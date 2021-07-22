1e7e2fd596138cd387dc27bd38f4d34a98f2e9c2
samuellup <samuel.d.lup@gmail.com>
2021-07-22 19:42:39 +0200

    .

--- scripts_snp/extend-qtl-variants-info.py
+++ scripts_snp/extend-qtl-variants-info.py
@@ -27,9 +27,9 @@ with open(args.output, "w") as out:
 				try: 
 					af_mut, af_wt, daf = str(dic_af[va_id][0])[:6], str(dic_af[va_id][1])[:6], str(dic_af[va_id][2])[:6]
 					out.write(
-						mut_type + "\t" + cont + "\t" + pos + "\t" + sp[3] + "\t" + sp[4] + "\t" + sp[5] + "\t" +  sp[6] + "\t" + sp[7] + "\t" + af_mut + "\t" +
-						af_wt + "\t" + daf + "\t" + sp[9] + "\t" + sp[10] + "\t" + sp[11] + "\t" + sp[12] + "\t" + sp[13] + "\t" + sp[14] + "\t" + sp[15] + "\t" + sp[16] + "\t" + sp[17] + "\t" +
-						sp[18] + "\t" + sp[19] + "\t" + sp[20] + "\t" + sp[21] + "\t" + sp[22] + "\t" + sp[23] + "\t" + sp[24] + "\t" + sp[25] + "\n"
+						mut_type + "\t" + cont + "\t" + pos + "\t" + str(sp[3]) + "\t" + str(sp[4]) + "\t" + str(sp[5]) + "\t" +  str(sp[6]) + "\t" + str(sp[7]) + "\t" + af_mut + "\t" +
+						af_wt + "\t" + daf + "\t" + str(sp[9]) + "\t" + str(sp[10]) + "\t" + str(sp[11]) + "\t" + str(sp[12]) + "\t" + str(sp[13]) + "\t" + str(sp[14]) + "\t" + str(sp[15]) + "\t" + str(sp[16]) + "\t" + str(sp[17]) + "\t" +
+						str(sp[18]) + "\t" + str(sp[19]) + "\t" + str(sp[20]) + "\t" + str(sp[21]) + "\t" + str(sp[22]) + "\t" + str(sp[23]) + "\t" + str(sp[24]) + "\t" + str(sp[25]) + "\n"
 					)
 				except: pass
 
@@ -42,4 +42,17 @@ candidate variants input columns:
 output columns: 
 @type\tcontig\tposition\tref_base\talt_base\talt_quality\tref_test_count\talt_test_count\talt_allele_freq_test\talt_allele_freq_control\tdAF\tdist_to_qtl_center\thit\tmrna_start\tmrna_end\tstrand\tgene_model\tgene_element\taa_pos\taa_ref\taa_alt\tgene_funct_annot\tf_primer\ttm_f_primer\tr_primer\ttm_r_primer\tupstream_sequence\tdownstream_sequence
     0	1		2			3		4			5			6				7				8						9					10		11					12	13			14			15		16			17			18		19		20		21					22		23			24			25			26					27
+'''
+
+
+# Saving this block for testing
+'''
+try: 
+	af_mut, af_wt, daf = str(dic_af[va_id][0])[:6], str(dic_af[va_id][1])[:6], str(dic_af[va_id][2])[:6]
+	out.write(
+		mut_type + "\t" + cont + "\t" + pos + "\t" + sp[3] + "\t" + sp[4] + "\t" + sp[5] + "\t" +  sp[6] + "\t" + sp[7] + "\t" + af_mut + "\t" +
+		af_wt + "\t" + daf + "\t" + sp[9] + "\t" + sp[10] + "\t" + sp[11] + "\t" + sp[12] + "\t" + sp[13] + "\t" + sp[14] + "\t" + sp[15] + "\t" + sp[16] + "\t" + sp[17] + "\t" +
+		sp[18] + "\t" + sp[19] + "\t" + sp[20] + "\t" + sp[21] + "\t" + sp[22] + "\t" + sp[23] + "\t" + sp[24] + "\t" + sp[25] + "\n"
+	)
+except: pass
 '''
